3c9dbbae55654ac843027398ce6d7e2460d89bea
wewwewdead <johnmathewloren27@gmail.com>
2024-12-10 22:27:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const photos = document.querySelectorAll('.photos');
-const bg = document.querySelector('body');
 
+const photos = document.querySelectorAll('.photos');
+const artWork = document.querySelector('.artwork');
+const dropdown = document.querySelector('.dropdown');
+const dropdownActivate = document.querySelector('.dropdown-activate');
+
+artWork.addEventListener('click', e => {
+    document.addEventListener('click', e => {
+        if(!dropdown.contains(e.target) && !artWork.contains(e.target)) { 
+            dropdown.classList.remove('dropdown-activate');
+        }
+    });
+    dropdown.classList.toggle('dropdown-activate');
+   
+});
 photos.forEach(photo => {
     photo.addEventListener('click', e => {
         document.addEventListener('click', e=> {
@@ -11,3 +23,5 @@ photos.forEach(photo => {
         photo.classList.toggle('zoom');
     })
 });
+
+
